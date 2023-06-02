565d283a3b12189ed96a95802c88bcf33e985934
adperalta <adperalta@isa.edu.do>
2023-06-02 13:59:04 -0400

    nueva remodelacion

--- Logica/main.py
+++ Logica/main.py
@@ -3,6 +3,7 @@ import shutil
 import panda as pd
 from docx.shared import Mm
 import docxtpl
+import csv
 
 import Carta
 import Directivo
@@ -23,6 +24,7 @@ EXCEL_PATH = "..\inputs\Templates\plantilla.xlsx"
 
 ########################## Funciones ###################################
 
+''''Crear carpeta de salidas'''
 def EliminarCrearCarpetas(path):
     #verificar si la carpeta existe y eliminarla
     if(os.path.exists(path)):
@@ -30,8 +32,24 @@ def EliminarCrearCarpetas(path):
     
     #Crear carpeta de salida
     os.mkdir(OUTPUT_PATH)
-    
+ 
+'''Crear Csv para plantilla de datos de las cartas'''
+def CrearCsV():
 
+    headers =['ID','Name','Last Name','Sexo','Email']
+    Users =[
+        ['2012-0990', 'Adrian','Peralta','M', 'user1@pywombat.com'],
+        ['2011-1516', 'Pamela','Peralta','F', 'user2@pywombat.com'],
+        ['2022-2203', 'John','Peralta','M','user3@pywombat.com'],
+        ['2023-5569', 'Josefa','Peralta','F', 'user3@pywombat.com'],
+    ]
+
+    with open('users.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(headers) # Creamos las columnas
+
+    for user in Users:
+        writer.writerow(user)
 
 ########################## /Funciones ###################################
 
